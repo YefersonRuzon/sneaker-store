4cbc6b4e8c0b2568fb342b9b162a0ed4a3301466
YefersonRuzon <jeffersonruzon@gmail.com>
2022-07-14 09:38:45 -0500

    landing

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
-
+import './Footer.scss'
 const Footer = () => {
     return (
         <>
-            <footer className={style.footer}>
-                <img src={logo} alt="#" />
-                <p>Yeferson Ruzón</p>
-                <div className={style.SocialContainer}>
+            <footer className="footer">
+                <h3>Snea <br /><span>k</span>ers.</h3>
+                <p>Social media</p>
+                <div className="SocialContainer">
                     <i class="ri-linkedin-fill"></i>
                     <i class="ri-dribbble-line"></i>
                     <i class="ri-instagram-line"></i>
                     <i class="ri-twitter-line"></i>
                     <i class="ri-youtube-line"></i>
                 </div>
-                <div className={style.info}>
-                    <p> Yeferson <span>Ruzon</span> - React<span>JS</span> - <i class="ri-copyright-line"></i>2022</p>
+                <span className='bgLine'></span>
+                <div className="info">
+                    <p> Yeferson Ruzon - ReactJS - <i class="ri-copyright-line"></i>2022</p>
                 </div>
             </footer>
         </>
